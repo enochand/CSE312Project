3264aa6336e43fd7f230c8195e0216ddbbdb5928
Josh Friedland <jefriedl@buffalo.edu>
2023-03-04 16:51:37 -0500

    new routes
    added signup form/route, and route to view user by id

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from flask import Flask, render_template
-import pymongo
+from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 
+mongo_client = MongoClient('mongo')
+db = mongo_client['excaliber']
+users = db['users']
+counter = db["counter"]
+counter.insert_one({"num_users": 0})
 app = Flask(__name__)
 
 
 @app.get('/')
 def landing():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+#add protection from html injection
+#add password encryption
+@app.post('/signup')
+def new_user():
+    username = request.form['new_username']
+    email = request.form['new_email']
+    counter.update_one({}, {"$inc": {"num_users": 1}})
+    users.insert_one({"id": counter.find_one()["num_users"], "username": username, "email": email})
+    return f"User {username} registered successfully"
+
+
+@app.get('/user/<user_id>')
+def user_info(user_id):
+    user = users.find_one({"id": int(user_id)})
+    if user is not None:
+        return str({"id": user["id"], "username": user["username"], "email": user["email"]})
 
 
 if __name__ == "__main__":
